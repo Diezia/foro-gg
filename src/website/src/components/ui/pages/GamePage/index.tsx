49239b5a1b0340757d59aa89ce34fe0ebf358fca
guidoviani27 <guido.viani27@gmail.com>
2022-08-09 22:41:30 -0300

    changes scss files

--- src/website/src/components/ui/pages/GamePage/index.tsx
+++ src/website/src/components/ui/pages/GamePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { fakePostsDataCSGO } from "../../../../helpers/fakePostsDataCSGO";
 import { PostPreview } from "../../PostPreview";
+import "../../../../styles/components/_gamepage.scss"
 
 export function GamePage() {
 	const { gameId } = useParams();
