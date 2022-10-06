102219df20e6bf662f29fcfb36fac7820f8c2288
diezia <diezyael@gmail.com>
2022-10-06 16:58:35 -0300

    fix unexpected logout

--- src/website/src/components/ui/GameBlock.tsx
+++ src/website/src/components/ui/GameBlock.tsx
@@ -2,6 +2,12 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/components/_gameblock.scss";
 import { Context, RefContext } from "../main/App";
+import jwtDecode from "jwt-decode";
+
+interface ITokenData {
+	name: string;
+	id: number;
+}
 
 interface GameBlockTypes {
 	nameGame: string;
@@ -12,18 +18,23 @@ interface GameBlockTypes {
 export function GameBlock({ imageSrc, gameUrl, nameGame }: GameBlockTypes) {
 	const { register, login } = useContext(RefContext);
 	const { state, dispatch } = useContext(Context);
-	console.log("otro tok", state.token);
+
 	return (
 		<>
-			{state.token ? (
+			{localStorage.getItem("jwt") ? (
 				<div className="grid-container__div-img">
 					<Link to={`games/${gameUrl}`}>
 						<img className="grid-container__img" src={imageSrc} alt={nameGame} />
 					</Link>
 				</div>
 			) : (
-				<div className="grid-container__div-img" onClick={() => {login.current.showModal()}}>
-						<img className="grid-container__img" src={imageSrc} alt={nameGame} />
+				<div
+					className="grid-container__div-img"
+					onClick={() => {
+						login.current.showModal();
+					}}
+				>
+					<img className="grid-container__img" src={imageSrc} alt={nameGame} />
 				</div>
 			)}
 		</>
